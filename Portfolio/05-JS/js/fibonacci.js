e7a13e42b8308c11199d885d5c31e04f8c82c48f
angel <sistemasaycbecarios@gmail.com>
2025-10-15 23:56:35 -0600

    Fibonacci code working

--- Portfolio/05-JS/js/fibonacci.js
+++ Portfolio/05-JS/js/fibonacci.js
@@ -8,20 +8,25 @@ function fibonacci() {
   "use strict";
   var n = document.getElementById("num").value;
   var val = f(n);
+  document.getElementById("fibonacciLbl").textContent = "The fibonacci value is " + val;
   return val;
 }
 
 function f(n) {
+  n = parseInt(n, 10);
   var value;
   // Check if the memory array already contains the requested number
   if (memo.hasOwnProperty(n)) {
     value = memo[n];
   } else {
     //TODO: Implement the fibonacci function here!
-
+    if (n < 2) {
+      value = n;
+    } else {
+      value = f(n - 1) + f(n - 2);
+    }
     memo[n] = value;
   }
 
   return value;
 }
-console.log(fibonacci(15));
